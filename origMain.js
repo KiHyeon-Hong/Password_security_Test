245200a6e741e6c69f05774d1fd69d34d0391b35
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-10-28 16:14:59 +0900

    update code

--- origMain.js
+++ origMain.js
@@ -7,6 +7,7 @@ for (let i = 1; i <= 5; i++) {
 
     let zxcvbn = [];
     let luds = [];
+    let leven = [];
     let labels = [];
     datas.map((data, index) => {
         if (index == 0) {
@@ -14,16 +15,18 @@ for (let i = 1; i <= 5; i++) {
         }
         zxcvbn.push(parseInt(data.split(',')[1]));
         luds.push(parseInt(data.split(',')[2]));
+        leven.push(parseInt(data.split(',')[3]));
         labels.push(parseInt(data.split(',')[4]));
     });
 
     let zxcvbnTemp = SimpleROC.simpleROC(zxcvbn, labels, 100);
     let ludsTemp = SimpleROC.simpleROC(luds, labels, 100);
+    let levenTemp = SimpleROC.simpleROC(leven, labels, 100);
 
     console.log(zxcvbnTemp[2]);
     console.log(ludsTemp[2]);
+    console.log(levenTemp[2]);
 
-    /*
     fs.writeFileSync(__dirname + `/GraphData/zxcvbnGraphDataX.txt`, ``, 'utf8');
     fs.writeFileSync(__dirname + `/GraphData/zxcvbnGraphDatay.txt`, ``, 'utf8');
     for (let j = 0; j < zxcvbnTemp[0].length; j++) {
@@ -47,5 +50,16 @@ for (let i = 1; i <= 5; i++) {
             fs.appendFileSync(__dirname + `/GraphData/ludsGraphDataY.txt`, `,`, 'utf8');
         }
     }
-    */
+
+    fs.writeFileSync(__dirname + `/GraphData/levenGraphDataX.txt`, ``, 'utf8');
+    fs.writeFileSync(__dirname + `/GraphData/levenGraphDatay.txt`, ``, 'utf8');
+    for (let j = 0; j < ludsTemp[0].length; j++) {
+        fs.appendFileSync(__dirname + `/GraphData/levenGraphDataX.txt`, `${levenTemp[0][j]}`, 'utf8');
+        fs.appendFileSync(__dirname + `/GraphData/levenGraphDataY.txt`, `${levenTemp[1][j]}`, 'utf8');
+
+        if (ludsTemp[0].length - 1 != j) {
+            fs.appendFileSync(__dirname + `/GraphData/levenGraphDataX.txt`, `,`, 'utf8');
+            fs.appendFileSync(__dirname + `/GraphData/levenGraphDataY.txt`, `,`, 'utf8');
+        }
+    }
 }
